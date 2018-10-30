9e08a3db0f5b014831b9c334b02098f31149bd67
bedenkog <bedenkog@coventry.ac.uk>
2018-10-30 16:34:59 +0000

    SearchRestaurants and BookRestaurant created

---
+++ ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/BookRestaurantActivity.java
@@ -0,0 +1,13 @@
+package com.bedenko.genaro.expresstable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_restaurant);
+    }
+}
