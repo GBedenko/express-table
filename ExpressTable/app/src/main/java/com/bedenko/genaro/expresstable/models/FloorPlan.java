2210bc3b65af1d98bec4a9194ceecf4444eed1fe
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-07 10:40:50 +0000

    Remaining entity classes populated

--- ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/models/FloorPlan.java
+++ ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/models/FloorPlan.java
@@ -1,4 +1,45 @@
 package com.bedenko.genaro.expresstable.models;
 
 public class FloorPlan {
+
+    private int floorPlanID;
+    private int restaurantID;
+    private int imageID;
+
+    public FloorPlan() {
+
+        // Can create a blank instance if no parameters passed
+    }
+
+    public FloorPlan(int floorPlanID, int restaurantID, int imageID) {
+
+        // Create a new instance when all parameters are passed
+        this.floorPlanID = floorPlanID;
+        this.restaurantID = restaurantID;
+        this.imageID = imageID;
+    }
+
+    public int getFloorPlanID() {
+        return floorPlanID;
+    }
+
+    public void setFloorPlanID(int floorPlanID) {
+        this.floorPlanID = floorPlanID;
+    }
+
+    public int getRestaurantID() {
+        return restaurantID;
+    }
+
+    public void setRestaurantID(int restaurantID) {
+        this.restaurantID = restaurantID;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
 }
--- ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/models/FoodOrder.java
+++ ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/models/FoodOrder.java
@@ -1,4 +1,35 @@
 package com.bedenko.genaro.expresstable.models;
 
 public class FoodOrder {
+
+    private int bookingID;
+    private String[] foodOrderArray;
+
+    public FoodOrder() {
+
+        // Can create a blank instance if no parameters passed
+    }
+
+    public FoodOrder(int bookingID, String[] foodOrderArray) {
+
+        // Create a new instance when all parameters are passed
+        this.bookingID = bookingID;
+        this.foodOrderArray = foodOrderArray;
+    }
+
+    public int getBookingID() {
+        return bookingID;
+    }
+
+    public void setBookingID(int bookingID) {
+        this.bookingID = bookingID;
+    }
+
+    public String[] getFoodOrderArray() {
+        return foodOrderArray;
+    }
+
+    public void setFoodOrderArray(String[] foodOrderArray) {
+        this.foodOrderArray = foodOrderArray;
+    }
 }
