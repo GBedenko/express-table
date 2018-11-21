77a21ea85c4b7b2d6770627baee3a130b30f5ecb
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-21 10:52:05 +0000

    Adding a booking to firebase function ready

--- ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/controllers/BookingController.java
+++ ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/controllers/BookingController.java
@@ -1,8 +1,15 @@
 package com.bedenko.genaro.expresstable.controllers;
 
 import com.bedenko.genaro.expresstable.models.Booking;
+import com.bedenko.genaro.expresstable.persistence.FirebaseHandler;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class BookingController {
+
+    FirebaseHandler firebaseHandler = new FirebaseHandler();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference bookingsCollection = db.collection("bookings");
 
     public Booking createBooking(int aCustomerID, int aRestaurantID, String aDate, String aTime, int aTableNumber) {
 
@@ -15,6 +22,12 @@ public class BookingController {
         booking.setTableNumber(aTableNumber);
 
         return(booking);
+    }
+
+    public void addBookingToFirebase(Booking booking) {
+
+        firebaseHandler.addDocumentToFirebase(booking, bookingsCollection);
+
     }
 
 }
