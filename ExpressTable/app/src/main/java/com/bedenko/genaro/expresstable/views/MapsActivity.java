1030fe8a0880e2658cd20934fd3db704ab175825
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-26 13:31:45 +0000

    Dynamically add markers to google maps screen

--- ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/views/MapsActivity.java
+++ ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/views/MapsActivity.java
@@ -25,23 +25,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add markers from stored restaurants
+        LatLng pizza_hut = new LatLng(52.4082385,-1.5155974);
+        mMap.addMarker(new MarkerOptions().position(pizza_hut).title("Pizza Hut"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pizza_hut));
     }
 }
