c664cfe912226ab7f2bdf0536adb0196271821b7
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-12-05 22:30:01 +0000

    Comments to explain data persistence handlers

--- ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/persistence/FirebaseHandler.java
+++ ExpressTable/app/src/main/java/com/bedenko/genaro/expresstable/persistence/FirebaseHandler.java
@@ -26,16 +26,20 @@ import javax.security.auth.callback.Callback;
 
 public class FirebaseHandler {
 
+    // Define TAG used for debugging purpose
     private static final String TAG = "FirebaseHandler";
+
+    // Class variables used for status of Firebase queries
     private boolean addedToFirebase = false;
     private boolean documentExists = false;
-
     private Map<String, Object> foundBookings;
-
     private ArrayList<Booking> bookingsList = new ArrayList<>();
 
+    // Using an object of any type, store it in Firebase in the provided CollectionReference
     public void addDocumentToFirebase(Object object, CollectionReference collectionReference) {
 
+        // Get the Firebase collection and add the provided object to it
+        // If successful, change the boolean to reflect it was successfully added
         collectionReference.document()
                 .set(object)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -54,11 +58,14 @@ public class FirebaseHandler {
                 });
     }
 
+    // Function to run a given query and return true if the document being queried exists
     public boolean queryIfDocumentExists(Query query) {
 
         // Ensures false as default before running query
         setDocumentExists(false);
 
+        // Using the provided query, run it and if successful the boolean
+        // stating if it exists will be true
         query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -72,21 +79,31 @@ public class FirebaseHandler {
             }
         });
 
+        // Return the value of the boolean after running the query
         return isDocumentExists();
     }
 
+    // Using the collection provided and a restaurant name
+    // Returns an array of all bookings assigned to the given restaurant
     public ArrayList<Booking> findBookings(CollectionReference collectionReference, String restaurantID) throws InterruptedException {
 
+        // Create a Firebase query which will find all bookings where the restaurantID matches the provided one
         Task<QuerySnapshot> snapshots = collectionReference.whereEqualTo("restaurantID", Integer.parseInt(restaurantID)).get();
 
-        // Sleep to ensure result of query is recieved by the device
+        // Sleep to ensure result of query is received by the device
         Thread.sleep(3000);
 
+        // For every booking record in the collection, add it to the array list of bookings
         for (QueryDocumentSnapshot document : snapshots.getResult()) {
+
+            // Log to be able to view which booking was found
             Log.d(TAG, "Data: " + document.getData());
+
+            // Append Booking object from Firebase to the array list
             bookingsList.add(document.toObject(Booking.class));
         }
 
+        // Return the array list of retrieved bookings
         return getBookingsList();
     }
 
